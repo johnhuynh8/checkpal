7401e8eb004f7efd873f5fc4ad1579dcdfebee5a
ahmedsh220 <52730061+ahmedsh220@users.noreply.github.com>
2019-10-19 14:42:29 -0400

    Add files via upload

---
+++ checkpal/connect.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+	<title>Checkpal | Financial Buddy</title>
+</head>
+
+<body>
+	<div class="container">
+		<form method="post" action="connect.php">
+			What is your Name : <input type="text" name="username" placeholder="Enter Your Name" /><br />
+			<input type="submit" value="Submit" />
+		</form>
+
+	</div>
+	<div class="container">
+		<img class="robo" src="images/robot.png">
+	</div>
+	<div class="container">
+		<button class="button" id="button2"> Back </button>
+		<button class="button" id="button1" onclick='alert("hello world")'>Next</button>
+	</div>
+</body>
+
+</html>
